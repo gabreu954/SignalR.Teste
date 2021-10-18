b08a6a28295059af424b18fff3bf77fc8cd20099
Gabriel <gabreu954@outlook.com>
2021-10-18 20:48:00 -0300

    upload solution - initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalR.Teste.Hubs;
+using SignalR.Teste.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Teste.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<ChatHub> _context;
+
+        public HomeController(ILogger<HomeController> logger, IHubContext<ChatHub> context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Teste.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message, int contador)
+        {
+            Clients.Caller.SendAsync("ReceiveMessage", message, contador);
+        }
+    }
+}
